af9c3641e441aa388b24f23a1d3b631a992a22e4
018798 <100913028+018798@users.noreply.github.com>
2026-06-02 19:39:33 +1000

    Debug: find java path in image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
-cat > /opt/traccar/conf/traccar.xml << EOF
-<?xml version='1.0' encoding='UTF-8'?>
-<!DOCTYPE properties SYSTEM 'http://java.sun.com/dtd/properties.dtd'>
-<properties>
-  <entry key='database.driver'>org.postgresql.Driver</entry>
-  <entry key='database.url'>jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_NAME}</entry>
-  <entry key='database.user'>${DB_USER}</entry>
-  <entry key='database.password'>${DB_PASSWORD}</entry>
-  <entry key='logger.level'>INFO</entry>
-  <entry key='logger.file'>/dev/stdout</entry>
-  <entry key='h02.port'>5027</entry>
-</properties>
-EOF
-exec /opt/java/bin/java -jar /opt/traccar/lib/tracker-server.jar /opt/traccar/conf/traccar.xml
+echo "=== Finding Java ==="
+find / -name "java" -type f 2>/dev/null | head -10
+echo "=== PATH: $PATH ==="
+sleep 60
